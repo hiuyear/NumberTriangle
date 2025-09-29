13f9e515a0c8952705fd208e75cdd6129ecc23ed
hiuyear <161685954+hiuyear@users.noreply.github.com>
2025-09-29 17:39:30 -0400

    finished method: retrieve()

--- src/main/java/NumberTriangle.java
+++ src/main/java/NumberTriangle.java
@@ -88,9 +88,37 @@ public class NumberTriangle {
      *
      */
     public int retrieve(String path) {
-        // TODO implement this method
-        return -1;
+        // td implement this method
+        if (path.isEmpty()) {
+            return this.root;
+        } else {
+            NumberTriangle curr = this;  // SUPER HARD! RMB THIS PART!!! SET CURR AS CURR STRUCTURE!!!
+            char[] chars = path.toCharArray();
+            for (char c : chars) {
+                if (c == 'l' && curr.left != null) {
+                    curr = curr.left;
+                } else if (c == 'r' && curr.right != null) {  // NOT ELIF ANYMORE!!!
+                    curr = curr.right;
+                }
+
+            }
+            return curr.root;
+        }
     }
+
+    // smarter method:
+//    public int retrieve(String path) {
+//        if (path.isEmpty()) {
+//            return this.root;
+//        }
+//        char step = path.charAt(0);
+//        if (step == 'l' && left != null) {
+//            return left.retrieve(path.substring(1));
+//        } else if (step == 'r' && right != null) {
+//            return right.retrieve(path.substring(1));
+//        }
+//        throw new IllegalArgumentException("Invalid path: " + path);
+//    }
 
     /** Read in the NumberTriangle structure from a file.
      *
